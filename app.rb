d3a24d5907574ab53e6fd6025a567b98a9c45aae
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-09-19 15:34:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,4 +25,21 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @operation = params[:operation]
+    case @operation
+    when 'add'
+      "#{@num1 + @num2}"
+    when 'multiply'
+      "#{@num1 * @num2}"
+    when 'subtract'
+      "#{@num1 - @num2}"
+    when 'divide'
+      "#{@num1 / @num2}"
+    end
+
+  end
+
 end
